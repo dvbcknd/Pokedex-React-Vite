8da0423f71fcc01f366bbff48169e60ca4106de0
dvbcknd <189028215+dvbcknd@users.noreply.github.com>
2026-05-30 21:43:08 -0300

    UPDAT: Foi finalizado a lógica da busca da API e feito o tratamento de dados para a exibição do pokemon, que foi enviado para o componente de exibir via props. Foi feito alguns ajustes também no css para melhor visualização.

--- src/components/captura/Captura.jsx
+++ src/components/captura/Captura.jsx
@@ -3,29 +3,25 @@ import React, { useState, useEffect, use } from 'react'
 import style from '../captura/Captura.module.scss'
 
 
-const GetApiPokemon = () => {
+function Captura() {
 
-  const [idDigitado, setIdDigitado] = useState(''); // O que o suário digita
   const [idBusca, setIdBusca] = useState(null); // O ID que dispara a busca
   const [dataApi, setDataApi] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (!idBusca) return;
-
+    if (!idBusca) return
     async function getApi() {
       try {
         setError(null);
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${idBusca}`);
-                    
+                      
         if (!response.ok) {
-          throw new Error("Pokémon não encontrado.");
+            throw new Error("Pokémon não encontrado.");
         }
-                    
+        
         const data = await response.json();
-        console.log(data)
         setDataApi(data);
-
       } catch (err) {
           setError(err.message);
           setDataApi(null);
@@ -34,57 +30,65 @@ const GetApiPokemon = () => {
       }
     }
 
-    getApi();
-  },[idBusca]);
+    getApi()
+  }, [idBusca])
 
-
-  return (
-    <section className={style.containerGetPokemonApi}>
-
-      <div className={style.preencher}>
-        <p>Número Pokemon:</p>
-        <input type="number" placeholder="EX: 1" value={idDigitado} onChange={ (e)=> setIdDigitado(e.target.value)}/>
-      </div>
-
-      <div className={style.buttons}>
-        <button className={style.buttonAuto} onClick={()=> setIdBusca(idDigitado)}>Auto preencher</button>
-        <button className={style.buttonRegistrar}>Registrar Pokémon</button>
-      </div>
-
-    </section>
-    
-  )
+    return (
+      <section className={style.containerCaptura}>
+        <GetApiPokemon setIdBusca={setIdBusca} />
+        <ExibirPokemon dataApi={dataApi} error={error} />
+      </section>
+    )
 }
 
 
 
-const ExibirPokemon = () => {
+const GetApiPokemon = ( {setIdBusca} ) => {
+  const [idDigitado, setIdDigitado] = useState(); // O que o suário digita
+   
+  return (
+    <section className={style.containerGetPokemonApi}>
+      <div className={style.preencher}>
+        <p>Número Pokemon:</p>
+        <input type="number" placeholder="EX: 1" value={idDigitado} onChange={ (e)=> setIdDigitado(e.target.value)}/>
+      </div>
+      <div className={style.buttons}>
+        <button className={style.buttonAuto} onClick={()=> setIdBusca(idDigitado)}>Auto preencher</button>
+        <button className={style.buttonRegistrar}>Registrar Pokémon</button>
+      </div>
+    </section>
+  )
+}
+
+
+const ExibirPokemon = ( {dataApi, error} ) => {
+
+  if (error) return <p className={style.mensagemErro}> Pokémon não encontrado... </p>
+  if (!dataApi) return <p className={style.mensagem}> Nenhum Pokémon pesquisado... </p>
+
+  const nome =  dataApi.name.toUpperCase();
+  const tipoPrincipal = dataApi.types[0].type.name;
+  const tipoSecundario = dataApi.types[1]?.type.name;
+  const img = dataApi.sprites.other['official-artwork'].front_default;
 
   return(
     <section className={style.containerExibirPokemon}>
 
       <div className={style.divInfos}>
-          <h3>NOME</h3>
-          <p className={style.nome}>Bulbassaur</p>
-          <h3>TIPO</h3>
-
-          <div className={style.containerTipos}>
-
-            <span className={style.tipoPrincipal}>
-              Grass
-            </span>
-
-            <span className={style.tipoSecundario}>
-              Poison
-            </span>
-
-          </div>
-
+        <h3>NOME</h3>
+        <p className={style.nome}>{nome}</p>
+        <h3>TIPO</h3>
+        <div className={style.containerTipos}>
+          <span className={style.tipoPrincipal}> {tipoPrincipal} </span>
+            {tipoSecundario
+              ? <span className={style.tipoSecundario}> {tipoSecundario} </span>
+              : <span className={style.tipoSecundario} style={{visibility: 'hidden'}}>-</span>
+            }
+        </div>
       </div>
-
       <div className={style.containerImg}>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-          alt="Bulbasaur" className=""/>
+        <img src={img}
+          alt={nome} className=""/>
       </div>
 
     </section>
@@ -92,14 +96,5 @@ const ExibirPokemon = () => {
 }
 
 
-
-function Captura() {
-  return (
-    <section className={style.containerCaptura}>
-      <GetApiPokemon />
-      <ExibirPokemon />
-    </section>
-  )
-}
 
 export default Captura
